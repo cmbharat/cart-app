1bd57beec7fd988c18b2a617da2fc9057f16b65a
cmbharat <bharatcm70@gmail.com>
2023-03-19 00:44:32 +0530

    Added increasing and decreasing quantity functionality in the CartItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import CartItem from "./CartItem";
+import Cart from "./Cart";
 
 function App() {
+  // function showAlert() {
+  //   alert("hello");
+  // }
+
+  // function handleInputChange(e) {
+  //   console.log(e.target.value);
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <CartItem />
+        <Cart />
+        {/* <button onClick={showAlert}></button> */}
+        {/* <input type="text" onChange={handleInputChange} /> */}
       </header>
     </div>
   );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,79 @@
 import React from "react";
+import { FaPlus, FaMinus, FaTrash } from "react-icons/fa";
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "mobile",
+      qty: 1,
+      img: "",
+    };
+  }
+
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    this.setState({
+      qty: this.state.qty + 1,
+      //   console.log("render")
+    });
+    // this.setState({
+    //   qty: this.state.qty + 1,
+    // });
+    // this.setState({
+    //   qty: this.state.qty + 3,
+    // });
+    // this.setState((prevState) => {
+    //   return { qty: prevState.qty + 1 };
+    // });
+  };
+
+  decreaseQuantity = () => {
+    this.setState((prevState) => {
+      if (prevState.qty == 0) return 0;
+      else
+        return {
+          qty: prevState.qty - 1,
+        };
+    });
+  };
+
   render() {
+    const { price, title, qty } = this.state;
+    console.log("render");
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img src="" alt="" style={styles.image} />
+          <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>Phone</div>
-          <div>Rs 999</div>
-          <div>Qty : 1</div>
-          <div className="cart-item-actions"></div>
+          <div style={{ color: "#777" }}>{title}</div>
+          <div>Rs {price}</div>
+          <div>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            <FaPlus
+              //   onClick={this.increaseQuantity.bind(this)}
+              //   onClick={() => {
+              //     // increaseQuantity(product);
+              //   }}
+              onClick={this.increaseQuantity}
+              className="action-items"
+              alt="Add"
+            />
+            <FaMinus
+              onClick={this.decreaseQuantity}
+              className="action-items"
+              alt="Decrease"
+            />
+            <FaTrash
+              onClick={() => {
+                // deleteQuantity(product.id);
+              }}
+              className="action-items"
+              alt="Delete"
+            />
+          </div>
         </div>
       </div>
     );
